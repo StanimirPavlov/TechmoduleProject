4bd4acefdb6a8d56b9886f64c916a3a55c0428b4
StanimirPavlov <stanimir.pavlov@abv.bg>
2017-04-18 00:30:51 +0300

    Add artilces summary on homepage and sort the articles

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,13 @@
 const Article = require('mongoose').model('Article');
 
 module.exports = {
+
   index: (req, res) => {
-      Article.find({}).limit(6).populate('author').then(articles => {
+
+      Article.find({}).sort({_id:-1}).limit(6).populate('author').then(articles => {
+          for(let article of articles){
+              article.content = article.content.substring(0,165);
+          }
         res.render('home/index', {
             articles
         });
